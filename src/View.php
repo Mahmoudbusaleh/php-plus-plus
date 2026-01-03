b846b86eb9fdf1d372b53f583e6ced1d96878495
Mahmoudbusaleh <99466747+Mahmoudbusaleh@users.noreply.github.com>
2026-01-03 11:45:10 +0300

    Add View class with render method for PHP views

---
+++ src/View.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PHPPlusPlus;
+
+class View {
+    /**
+     * Renders a PHP view file from the views directory.
+     */
+    public static function render($view, $data = []) {
+        $path = "views/{$view}.php";
+
+        if (!file_exists($path)) {
+            header("HTTP/1.0 500 Internal Server Error");
+            die("PHP++ Error: View [{$view}] not found in /views folder.");
+        }
+
+        // Makes array keys available as variables inside the view
+        extract($data);
+
+        // Capture output to return it as a string
+        ob_start();
+        include $path;
+        return ob_get_clean();
+    }
+}
